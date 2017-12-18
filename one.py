51cfe8e65d2e6b998f99031aa3a17ba3110f6b51
Tejas Pant <tejaspant55@gmail.com>
2017-12-18 14:55:28 +0545

    One Committed

--- one.py
+++ one.py
@@ -6,4 +6,5 @@ def new_salary(salary):
 print('Salary after raise: ')
 for index, each in enumerate(salary,start=1):
     raised_salary=new_salary(each)
+
     print('For employee {}, total salary = Rs. {:.2f}'.format(index, raised_salary))
